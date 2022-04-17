26b232cfd447758be38ad2525485b8017629291f
RennanD <rplaykl@gmail.com>
2022-04-16 22:53:11 -0300

    feat: sign up by web app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
+import { ToastContainer } from 'react-toastify';
 import '../styles/global.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,12 +1,40 @@
 import { FiMail, FiUser, FiLock, FiCheck } from 'react-icons/fi';
 
+import { FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 import styles from './styles.module.scss';
 
 import { TextInput } from '../../components/Forms/TextInput';
 import { Navbar } from '../../components/Navbar';
 import { Button } from '../../components/Forms/Button';
+import { api } from '../../services/api';
 
 export default function SignUp(): JSX.Element {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      await api.post('/users', { name, email, password });
+      toast('Conta criada com sucesso', {
+        theme: 'colored',
+        type: 'success',
+      });
+    } catch ({ response }) {
+      toast(response.data.error, {
+        theme: 'colored',
+        type: 'error',
+      });
+      console.log(response);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Navbar />
@@ -25,12 +53,14 @@ export default function SignUp(): JSX.Element {
           </span>
         </div>
 
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSignUp}>
           <h1>Formulário de cadastro</h1>
 
           <TextInput
             label="Nome"
             id="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
             icon={FiMail}
             placeholder="Nome completo..."
           />
@@ -38,19 +68,26 @@ export default function SignUp(): JSX.Element {
             label="E-mail"
             id="email"
             type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
             placeholder="email@exemplo.com"
             icon={FiUser}
           />
           <TextInput
             label="Senha"
             id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
             icon={FiLock}
             placeholder="********"
+            type="password"
           />
 
           <br />
 
-          <Button icon={FiCheck}>Criar conta</Button>
+          <Button type="submit" icon={FiCheck} loading={loading}>
+            Criar conta
+          </Button>
         </form>
       </section>
     </div>
