057f937d37ec8c55821908de5ebab7b75428f80a
RennanD <rennan.douglas@objetivopi.com.br>
2022-04-18 15:08:24 -0300

    chore: init app sidebar

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link';
+import { FiFolder, FiGrid, FiLogOut, FiUser } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export default function Sidebar(): JSX.Element {
+  return (
+    <aside className={styles.container}>
+      <div className={styles.logoContainer}>
+        <a href="">
+          <h1>
+            repodoc <span>.</span>
+          </h1>
+        </a>
+      </div>
+
+      <nav>
+        <strong className={styles.sectionTitle}>Geral</strong>
+
+        <ul className={styles.listLinks}>
+          <li className={styles.listItems}>
+            <Link href="/app/dashboard">
+              <a className={styles.activeLink}>
+                <FiGrid />
+                Painel
+              </a>
+            </Link>
+          </li>
+        </ul>
+
+        <strong className={styles.sectionTitle}>Aplicação</strong>
+        <ul className={styles.listLinks}>
+          <li className={styles.listItems}>
+            <Link href="/app/organizations">
+              <a>
+                <FiUser />
+                Organizações
+              </a>
+            </Link>
+          </li>
+
+          <li className={styles.listItems}>
+            <Link href="/app/organizations">
+              <a>
+                <FiFolder />
+                Documentações
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <footer>
+        <Link href="/app/organizations">
+          <a className={styles.logOutButton}>
+            <FiLogOut />
+            Sair da aplicação
+          </a>
+        </Link>
+      </footer>
+    </aside>
+  );
+}
--- src/pages/app/dashboard/index.tsx
+++ src/pages/app/dashboard/index.tsx
@@ -1,9 +1,14 @@
-// import styles from './styles.module.scss';
+import styles from './styles.module.scss';
+
+import Sidebar from '../../../components/Sidebar';
 
 export default function Dashboard(): JSX.Element {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className={styles.container}>
+      <Sidebar />
+      <main>
+        <h1>Dashboard</h1>
+      </main>
     </div>
   );
 }
